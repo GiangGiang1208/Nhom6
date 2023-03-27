cbd6111e37000426aa3855b86e294bfc8ef44d72
GiangGiang1208 <128273922+GiangGiang1208@users.noreply.github.com>
2023-03-27 21:18:40 +0700

    add controller
    add controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,82 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using TinTucCongNghe.Models;
+using TinTucCongNghe.ModelViews;
 
 namespace TinTucCongNghe.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly dbTinTucCongNgheContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, dbTinTucCongNgheContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeViewVM model = new HomeViewVM();
+
+            var lsPosts = _context.Posts
+                .AsNoTracking()
+                .Where(x => x.IsPublish == true && x.HomeFlag == true)
+                .OrderByDescending(x => x.DateCreated)
+                .ToList();
+            var lsCats = _context.Categories
+                .AsNoTracking()
+                .Where(x => x.IsPublish == true)
+                .OrderBy(x => x.Ordering)
+                .Take(2)
+                .ToList();
+
+            List<PostHomeVM> lsPostViews = new List<PostHomeVM>();
+            foreach (var item in lsCats)
+            {
+                PostHomeVM postHome = new PostHomeVM();
+                postHome.category = item;
+                postHome.lsPosts = lsPosts.Where(x => x.CatId == item.CatId).ToList();
+                lsPostViews.Add(postHome);
+            }
+
+            var lsTinHot = _context.Posts
+                .AsNoTracking()
+                .Where(x => x.IsPublish == true && x.IsHot == true)
+                .OrderByDescending(x => x.DateCreated)
+                .Take(6)
+                .ToList();
+
+            var lsTinMoi1 = _context.Posts
+                .AsNoTracking()
+                .Where(x => x.IsPublish == true && x.IsNew == true)
+                .OrderByDescending(x => x.DateCreated)
+                .Take(2)
+                .ToList();
+            var lsTinMoi2 = _context.Posts
+                .AsNoTracking()
+                .Where(x => x.IsPublish == true && x.IsNew == true)
+                .OrderByDescending(x => x.DateCreated)
+                .Skip(2)
+                .Take(2)
+                .ToList();
+
+            var quangcao = _context.Advs
+                .AsNoTracking()
+                .Where(x => x.IsActive == true)
+                .OrderByDescending(x => x.DateCreated)
+                .Take(5)
+                .ToList();
+
+            model.Posts = lsPostViews;
+            model.QuangCaos = quangcao;
+            model.TinHots = lsTinHot;
+            model.TinMois1 = lsTinMoi1;
+            model.TinMois2 = lsTinMoi2;
+            ViewBag.AllPosts = lsPosts;
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ Controllers/PostController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PagedList.Core;
+using System.Linq;
+using TinTucCongNghe.Models;
+
+namespace TinTucCongNghe.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly dbTinTucCongNgheContext _context;
+        public PostController(dbTinTucCongNgheContext context)
+        {
+            _context = context;
+        }
+        
+
+        public IActionResult Index(int? page)
+        {
+            try
+            {
+                var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+                var pageSize = 8;
+                var lsPost = _context.Posts.AsNoTracking()
+                    .Where(x => x.IsPublish == true && x.CatId == 1)
+                    .OrderByDescending(x => x.DateCreated);
+                PagedList<Post> models = new PagedList<Post>(lsPost, pageNumber, pageSize);
+                ViewBag.CurrentPage = pageNumber;
+                var category = _context.Categories.Where(x => x.IsPublish == true).ToList();
+                ViewBag.Categories = category;
+                return View(models);
+            }
+            catch
+            {
+                return NotFound();
+            }
+
+        }
+
+        [Route("{Alias}-{id}.html", Name = "PostDetails")]
+        public IActionResult Details(int id)
+        {
+            var post = _context.Posts.AsNoTracking().SingleOrDefault(x => x.PostId == id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        
+        public IActionResult List(string Alias, int page = 1)
+        {
+            try
+            {
+                var pageSize = 8;
+                var danhmuc = _context.Categories.AsNoTracking().SingleOrDefault(x => x.Alias == Alias);
+
+                var lsPost = _context.Posts
+                    .AsNoTracking()
+                    .Where(x => x.CatId == danhmuc.CatId)
+                    .OrderByDescending(x => x.DateCreated);
+                PagedList<Post> models = new PagedList<Post>(lsPost, page, pageSize);
+                ViewBag.CurrentPage = page;
+                ViewBag.CurrentCat = danhmuc;
+                var category = _context.Categories.Where(x => x.IsPublish).ToList();
+                ViewBag.Categories = category;
+                return View(models);
+            }
+            catch
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
