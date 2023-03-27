90db5520da270762cc4e39fbba24bf13dd20c040
GiangGiang1208 <128273922+GiangGiang1208@users.noreply.github.com>
2023-03-27 21:19:44 +0700

    add modelViews
    add modelViews

---
+++ ModelViews/HomeViewVM.cs
@@ -0,0 +1,14 @@
+﻿using TinTucCongNghe.Models;
+
+namespace TinTucCongNghe.ModelViews
+{
+    public class HomeViewVM
+    {
+        public List<Post> TinHots { get; set; }
+        public List<Post> TinMois1 { get; set; }
+        public List<Post> TinMois2 { get; set; }
+        public List<PostHomeVM> Posts { get; set; }
+        public List<Adv> QuangCaos { get; set; }
+
+    }
+}
---
+++ ModelViews/PostHomeVM.cs
@@ -0,0 +1,10 @@
+﻿using TinTucCongNghe.Models;
+
+namespace TinTucCongNghe.ModelViews
+{
+    public class PostHomeVM
+    {
+        public Category category { get; set; }
+        public List<Post> lsPosts { get; set; }
+    }
+}
